20ac043ab9c304b176d93286379f964140a91cbe
Flaviu Tamas <tamasflaviu@gmail.com>
2014-02-04 21:20:24 -0500

    Now can throw away microbenchmark results more genericly

--- test/factorygame/package.scala
+++ test/factorygame/package.scala
@@ -1,16 +1,16 @@
 package object factorygame {
-	var PreventOpt = 0
+	var PreventOpt: Any = 0.asInstanceOf[Any]
 
-	def compareFuncs(f1: () => Int, f2: () => Int) = {
+	def compareFuncs[T](f1: () => T, f2: () => T) = {
 		val t1 = timeFunc(f1)
 		val t2 = timeFunc(f2)
 		(t1, t2)
 	}
 
-	def timeFunc(f: () => Int) = {
+	def timeFunc[T](f: () => T) = {
 		val ret = 0.to(10).map(_ => {
 			val start = System.nanoTime()
-			PreventOpt += f()
+			PreventOpt = PreventOpt == f()
 			val stop = System.nanoTime()
 			(stop - start).toInt
 		})
